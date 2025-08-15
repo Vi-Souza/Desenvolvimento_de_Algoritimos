c20ef4ef1d0638d5aa945029024145b1682cddbe
Vi-Souza <vitorsr.souza02@gmail.com>
2025-08-15 23:14:11 +0000

    polindo

--- Aulas/aula2_exer1.c
+++ Aulas/aula2_exer1.c
@@ -4,20 +4,20 @@
 int main(){
     float nf, lab1, lab2, lab3, lab, pj, pv;
 
-    printf("Digite a nota do laboratório 1");
+    printf("Digite a nota do laboratório 1: ");
     scanf("%f", &lab1);
 
-    printf("Digite a nota do laboratório 2");
+    printf("Digite a nota do laboratório 2: ");
     scanf("%f", &lab2);
 
-    printf("Digite a nota do laboratório 3");
+    printf("Digite a nota do laboratório 3: ");
     scanf("%f", &lab3);
 
-    printf("Digite a nota do projeto");
+    printf("Digite a nota do projeto: ");
 
     scanf("%f", &pj);
 
-    printf("Digite a nota da prova final");
+    printf("Digite a nota da prova final: ");
 
     scanf("%f", &pv);
 
