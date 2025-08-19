99f1ced0529922193efe4e8ad838a3f0bc2aaa02
Vi-Souza <vitorsr.souza02@gmail.com>
2025-08-19 23:25:18 +0000

    Organizacao e exercicio 1 aula 2

---
+++ Exercicios_Aula2/calculadora.c
@@ -0,0 +1,14 @@
+#include "calculadora.h"
+
+float somar(float a, float b) {
+return a + b;
+}
+float subtrair(float a, float b) {
+return a - b;
+}
+float multiplicar(float a, float b) {
+return a * b;
+}
+float div(float a, float b) {
+return a / b;
+}
---
+++ Exercicios_Aula2/main1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "calculadora.h"
+
+int main() {
+float a = 10.0, b = 3.0;
+
+printf("a = %.2f, b = %.2f\n", a, b);
+printf("Soma: %.2f\n", somar(a, b));
+printf("Subtracao: %.2f\n", subtrair(a, b));
+printf("Multiplicacao: %.2f\n", multiplicar(a, b));
+
+if (eh_divisao_valida(b)) {
+printf("Divisao: %.2f\n", dividir(a, b));
+} else {
+printf("Erro: Divisao por zero!\n");
+}
+
+return 0;
+}
